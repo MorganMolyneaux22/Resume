ee78849556113240568681442dd961fbe7dfaf6f
Morgan Molyneaux <92762025+MorganMolyneaux22@users.noreply.github.com>
2023-11-21 00:58:53 -0700

    Install Tailwind, node.s, and flowbite
    Installing UI component builder

---
+++ script.js
@@ -0,0 +1,9 @@
+// var typed = new Typed('#typing', {
+//     strings: ["Morgan Molyneaux"],
+//     typeSpeed: 85,
+//     onComplete: function () {
+//         $('#typing').fadeOut(3500, function () {
+//             $('#resumeContent').fadeIn(1000);
+//         });
+//     }
+// });
